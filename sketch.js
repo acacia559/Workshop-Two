54a33b80f3b1d6e141d43c22d6135d08dcece5bb
acacia559 <afle559@aucklanduni.ac.nz>
2025-01-22 13:19:10 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+setTimeout (drawBear, 2000)
+  
+
+
+function drawBear () {
+  fill (150,75,0);
+  noStroke ();
+  circle (200, 200, 50);
+  circle (175, 195 , 25);
+  circle (225, 195, 25);
+}
